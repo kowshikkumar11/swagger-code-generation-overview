c189426be7acc2914f38c6ce679c3cfa71c83581
saikumar pedapudi <saikumar.pedapudi@wu.com>
2024-09-05 16:09:35 +0530

    initial commit

---
+++ src/main/java/com/example/swaggercodegenerationoverview/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.example.swaggercodegenerationoverview.controller;
+
+import com.example.swaggercodegenerationoverview.api.StudentApi;
+import com.example.swaggercodegenerationoverview.model.Student;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController implements StudentApi {
+
+
+    public ResponseEntity<Student> getStudentDetail(String studentId) {
+        // get student data from service layer
+        Student student = new Student();
+        student.setId(1);
+        student.setName("Vivek");
+        student.setAge(27);
+        ResponseEntity<Student> responseEntity = ResponseEntity.ok(student);
+        return responseEntity;
+    }
+
+    public ResponseEntity<Student> createStudentDetail(Student student) {
+        // create student record in DB and return same
+        return ResponseEntity.ok(student);
+    }
+}
